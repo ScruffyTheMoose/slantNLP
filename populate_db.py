0dd4b50d52eae3ca8f38773942df799548bafcd2
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2021-10-23 01:12:57 -0500

    Updated to current version
    Replaced the placeholder with script related to slantNLP goals. This file will eventually become a full module that will be called in the main module. For now, I'll be using simple scripts to set up the foundations of the DB.

--- populate_db.py
+++ populate_db.py
@@ -1,47 +1,22 @@
-# Leaving this as a placeholder for a modified version specifically for sentiment related data.
-# This is from a finance related algorithm I am building that is sending data to a SQLite db.
-
+import pandas as pd
 import sqlite3
-import json
-from urllib.request import urlopen
 
-url = "https://financialmodelingprep.com/api/v3/stock/list?apikey=*****"
+# Script to read in a CSV file of official hashtags for all NFL teams to the database for parent table
+# This will become a full module with callable methods in the near future, but this will do for setting up the foundations.
 
-def getParsedData(url):
-    response = urlopen(url)
-    data = response.read().decode("utf-8")
-    return json.loads(data)
+df = pd.read_csv("nfl_teams.csv")
 
-# symbols are located at each index under key "symbol" -> rawData[0]["symbol"]
-# company names are located under similar index and key -> rawData[0]["name"]
-rawData = getParsedData(url)
-
-# print(rawData[0])
-#==========================================
-
-connection = sqlite3.connect('app.db')
+connection = sqlite3.connect('nfl.db')
 
 cursor = connection.cursor()
 
-ec = 0
+for row in df.values:
 
-for i in range(0, len(rawData)-1):
-    tSymbol = rawData[i]["symbol"]
-    tCompany = rawData[i]["name"]
+    tag = row[0]
+    team = row[1]
 
-    if "'" in tCompany:
-        tCompany = tCompany.replace("'", "")
-
-
-    if "." not in rawData[i]["symbol"]:
-        try:
-            cursor.execute("INSERT INTO stock (symbol, company) VALUES ('{}', '{}')".format(tSymbol, tCompany))
-        except Exception as e:
-            ec += 1
-            print(ec)
-            print(rawData[i]["symbol"])
-            print(e)
-    else:
-        continue
+    cursor.execute("""
+    INSERT INTO nfl_teams (tag, team) VALUES ('{}', '{}')
+    """.format(tag, team))
 
 connection.commit()
