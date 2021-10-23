41eb84859d0ffab7b695e4878b56f9d71c2056be
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2021-10-23 01:10:07 -0500

    Updated to current version
    Updated to structure that is specific to slantNLP goals. Removed any unrelated placeholders.

--- create_db.py
+++ create_db.py
@@ -1,33 +1,32 @@
-# I am leaving this as a placeholder for the updated version for sentiment data.
-# This is (obviously) not a database for sentiment related data. This is from a separate finance related algorithm.
 # Please look at the 'processor' module to see what data will be gathered, analyzed, and it's dataframe output.
 
 import sqlite3
 
-connection = sqlite3.connect('app.db')
+# Script to create parent and child table for NFL related twitter data. Subject to change given different data related needs.
+
+connection = sqlite3.connect('nfl.db')
 
 cursor = connection.cursor()
 
 cursor.execute("""
-    CREATE TABLE IF NOT EXISTS stock (
+    CREATE TABLE IF NOT EXISTS nfl_teams (
         id INTEGER PRIMARY KEY,
-        symbol TEXT NOT NULL UNIQUE,
-        company TEXT NOT NULL UNIQUE
+        tag TEXT NOT NULL UNIQUE,
+        team TEXT NOT NULL UNIQUE
     )
 """)
 
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS stock_price (
         id INTEGER PRIMARY KEY,
-        stock_id INTEGER,
+        team_id INTEGER,
+        body TEXT NOT NULL,
+        sentiment NOT NULL,
+        tags NOT NULL,
+        keywords,
+        likes INTEGER,
         date NOT NULL,
-        open NOT NULL,
-        high NOT NULL,
-        low NOT NULL,
-        close NOT NULL,
-        adjusted_close NOT NULL,
-        volume NOT NULL,
-        FOREIGN KEY (stock_id) REFERENCES stock (id)
+        FOREIGN KEY (team_id) REFERENCES nfl_teams (id)
     )
 """)
 
