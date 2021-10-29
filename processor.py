8f3a80502535141643b125374ca15fd7358f6abc
ScruffyTheMoose <91182195+ScruffyTheMoose@users.noreply.github.com>
2021-10-28 20:45:42 -0500

    Restructure
    Large changes to Sentiment and Keywords classes. Removed outdated methods and replaced them with significantly faster and easier to manage operations.

--- processor.py
+++ processor.py
@@ -1,18 +1,10 @@
-from nltk.tag import pos_tag
-import pandas as pd
-import numpy as np
 from nltk.tokenize import RegexpTokenizer #to tokenize the text body
-from nltk.corpus import stopwords #to remove stopwords that do not add value to the analysis
 from nltk.stem import WordNetLemmatizer #to simplify words into more basic forms
-from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
-from nltk.stem import PorterStemmer
-from nltk import FreqDist
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import emoji
 import re #to remove links that may be in the text body
 import spacy
-import scispacy
 from string import punctuation
-from collections import Counter
 
 # Class that contains methods for cleaning input data to prepare for sentiment analysis
 class PreProcess:
@@ -21,14 +13,14 @@ class PreProcess:
         pass
 
     # Method that removes any emojies identified inside the textbody to help feed more clean data into NLP.
-    def removeEmoji(self, textBody):
+    def removeEmoji(textBody):
         string_emojiless = emoji.get_emoji_regexp().sub(u'', textBody) #removes all instances of emojis from textbody
         
         return string_emojiless #returns string
     
     # Method that will tokenize the text body and return the contents in a list form.
     # More effective and efficient that splitting the string.
-    def tokenizeString(self, emojiless_textbody):
+    def tokenizeString(emojiless_textbody):
         tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|http\S+')
 
         tokenized_string = tokenizer.tokenize(emojiless_textbody) #tokenizes textbody into a list
@@ -36,7 +28,7 @@ class PreProcess:
         return tokenized_string #returns list of words
 
     # Method that iterates through the list-form text, converts all values to lowercase, and return in a list form
-    def makeLowerCase(self, tked_list):
+    def makeLowerCase(tked_list):
         text_lowcase = []
 
         for word in tked_list: #iterates through tk text and makes all words lowercase
@@ -46,7 +38,7 @@ class PreProcess:
 
     # Method removes all identified stopwords from the textlist to further clean the data before NLP.
     # Stop words do not provide any information to the analysis.
-    def removeStopWords(self, LC_list):
+    def removeStopWords(LC_list):
         nlp = spacy.load('en_core_web_sm') #retrieving english data from spacy
         stopwords = nlp.Defaults.stop_words #getting known stopwords from enlish data
 
@@ -59,7 +51,7 @@ class PreProcess:
         return text_NoSW #returns list
 
     # Method lemmatizes words, putting them in a more basic and comprehensible format for NLP.
-    def lemmatizer(self, word_list):
+    def lemmatizer(word_list):
         lemmatizer = WordNetLemmatizer() #creating lemmatizer object
 
         lemmatized_tks = []
@@ -68,6 +60,15 @@ class PreProcess:
             lemmatized_tks.append(lemmatizer.lemmatize(word))
 
         return lemmatized_tks #returns list
+
+    # Method that will operate all preceding code blocks in sequence
+    # Returns list of lemmatized words from input text
+    def run(text):
+        stage1 = PreProcess.removeEmoji(text)
+        stage2 = PreProcess.tokenizeString(stage1)
+        stage3 = PreProcess.makeLowerCase(stage2)
+        stage4 = PreProcess.removeStopWords(stage3)
+        return PreProcess.lemmatizer(stage4)
         
 # Class that apply preprocessing methods and then analyzes the resulting text for sentiment.
 # Will output -1 or 1 at completion of process representing negative or positive sentiment
@@ -76,84 +77,27 @@ class Sentiment:
     def __init__(self) -> None:
         pass
 
-    # Method that calls all preProcessing methods at once and returns a simplified list of strings.
-    def preProcess(self, text):
-        prep = PreProcess()
+    # Method uses the VADER SentimentInstensityAnalyzer to evaluate the sentiment of the base text without cleaning.
+    # This gives the tool full access to the population of words rather than a cleaned text so that it may more accurately evaluate sentiment.
+    # VADER score (vs) is then evaluated for thresholds and returns result. This needs tweaking.
+    # Significantly faster than previous methods.
+    def sentiment(text):
+        analyzer = SentimentIntensityAnalyzer()
+        vs = analyzer.polarity_scores(text)
 
-        stepOne = prep.removeEmoji(text)
-        stepTwo = prep.tokenizeString(stepOne)
-        stepThree = prep.makeLowerCase(stepTwo)
-        stepFour = prep.removeStopWords(stepThree)
-        result = prep.lemmatizer(stepFour)
-
-        return result #returns list
-
-    # Method uses NLTK's VADER method for evaluating the polarity of each word from the cleaned input.
-    # Returns an organized DataFrame containing each word and it's assigned polarity.
-    # Any word with a polarity outside the (-0.10, 0.10) interval will be assigned -1 and 1 respectively.
-    # All words inside that interval are assigned a neutral value.
-    def sentiment(self, cleaned_text):
-        sia = SIA()
-        results = []
-
-        for phrases in cleaned_text: #apply VADER word by word
-            pol_score = sia.polarity_scores(phrases)
-            pol_score['words'] = phrases #saving the determined polarity to a dictionary
-            results.append(pol_score) #adding resulting k:v pair to list
-
-        pd.set_option('display.max_columns', None, 'max_colwidth', None)
-        df = pd.DataFrame.from_records(results) #converting the results list into a dataframe
-        df['label'] = 0 #adding a label column to simplify reading
-        df.loc[df['compound'] > 0.10, 'label'] = 1 #adding label positive
-        df.loc[df['compound'] < -0.10, 'label'] = -1 #adding label negative
-        
-        return df #returns dataframe
-
-    # Method that evaluates the dataframe to determine the sentiment skew.
-    # Returns a -1 or 1 for Negative or Positive skew respectively.
-    def findSkew(self, data_df): 
-        data_PosNeg = data_df.loc[data_df['label'] != 0] #filtering out words with neutral labels.
-        count = data_PosNeg.label.value_counts().idxmax() #determining value with highest occurrence
-
-        return count
-
+        return vs
 
 # Class that evaluates the keywords and keyphrases in the text being evaluated.
 # These phrases will be used to identify shared sentiment across a large number of posts.
-# Opted to use scispaCy pipeline since it is more effective at identifying scientific and medical terminology.
-# Target data is not science/medical, but having extra access is better than missing data.
+# Opted to use scispaCy pipeline since it is apparently more effective at identifying keywords in the target material.
 class Keywords:
 
     def __init__(self) -> None:
         pass
 
-    # Method parses input text, identifies keywords, and splits them into a list.
-    def keywords(self, text):
-        nlp = spacy.load('en_core_sci_sm') #loading spacy science pipeline. More effective at variety data
-        doc = nlp(text) #applying pipeline
-        pos_tag = ["PROPN", "ADJ", "NOUN"] #word types we want
-        result = []
-
-        for token in doc: #parsing keywords
-            if token.text in nlp.Defaults.stop_words or token.text in punctuation:
-                continue
-            
-            if token.pos_ in pos_tag:
-                result.append(token.text)
-
-        return result #returns list
-
-    # Method counts occurrences of words from list of keywords
-    def keywordCount(self, keyword_list):
-        return Counter(keyword_list).most_common(5) #returns dict
-
-    # Method identifies keyphrases in text based on spaCy scientific pipeline.
-    def keyphrases(self, text):
-        nlp = spacy.load('en_core_sci_sm') #loading pipeline
-        doc = nlp(text) #applying pipeline
-        result = []
-
-        for element in doc.ents: #converting spacy objects into individual lines of text in a list.
-            result.append(element)
-        return result #returns list
-
+    # Method identifies keyphrases in text by simply splitting the text body by punctuation.
+    # This is the most efficient way to break a text into English phrases without needed to rebuild sentences from ground up.
+    # These will be stores and used to evaluate common sentiment across numerous texts.
+    # This is also exponentially faster than previousl methods
+    def keyphrases(text):
+        return re.split("[?!,.:;]", PreProcess.removeEmoji(text).lower())
